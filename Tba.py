9efbcbbff0c05e226d99938de1d3cd216af0bf68
Bowthebro <85522775+Bowthebro@users.noreply.github.com>
2022-12-12 13:28:03 +1300

    PEP-8 styling fixes. Still does not work but messing around.

--- Tba.py
+++ Tba.py
@@ -1,193 +1,161 @@
-#TEXT BASED ADVENTURE OOP PYTHON PROGRAM
+# TEXT BASED ADVENTURE OOP PYTHON PROGRAM
 
-#defines level class
-class level():
+# defines Level class
+class Level:
     def __init__(self):
-        self.name = ("")
+        self.name = ""
+        self.description = None
+        self.gates = None
 
-    #level setup function
-    def setup(self, name, directions, description):
+    #  defines level setup function, defines instances of level class
+    def setup(self, name, gates, text):
         self.name = name
-        self.directions = directions
-        self.description = description
-        
+        self.gates = gates
+        self.description = text
 
+    # defines enter function, gives player info of area
     def enter(self):
         print(self.description)
-        if len(self.directions) >= 1:
-            print("there is a gate to ", end=" ")
-            for d in self.directions:
-                d.print_gate()
 
-#defines gate class
-class gate:
+        if len(self.gates) >= 1:
+            print("there is a gate to ", end=' ')
+            for g in self.gates:
+                g.print_gate()
+        print("\n", end='')
 
+
+# defines gate class
+class Gate:
+    
     def __init__(self, direction, gate_to):
         self.gate_to = gate_to
         self.direction = direction
 
+    # defines is gate function, returns direction of gate
     def is_gate(self, text):
         return self.direction in text
 
+    # defines print gate function, prints direction of gate
     def print_gate(self):
-        print("the %s," % self.direction, end= " ")
+        print("the %s," % self.direction, end=' ')
 
 
+# defines player class
+class Player:
 
-class player():
-
-    def __init__(self, name):
+    def __init__(self, name, player_level):
         self.health = 100
         self.name = name
-        self.location = level
+        self.location = player_level
 
-    def move(self, level):
-        self.location = level
-
-    def player_is_in_room(self, level):
-        return self.location == level
+    # defines move function, changes player location
+    def move(self, player_level):
+        self.location = player_level
 
 
-class sword():
-    damagetype= ""
-    type = ""
-    colour = ""
-    pickedup = False
+# defines sword class
+class Sword:
+    picked_up = False
 
-    def __init__(self, damagetype, type, colour):
-        self.damagetype = damagetype
-        self.type = type
+    def __init__(self, sword_type, damage_type, colour):
+        self.sword_type = sword_type
+        self.damage_type = damage_type
         self.colour = colour
 
-#initialises levels
-start_area = level()
-deep_forest_area = level()
+
+# initialises Levels
+start_area = Level()
+deep_forest_area = Level()
+
+# start area setup
+gate1 = Gate("north", deep_forest_area)
+start_area.setup("forest", [gate1], """You are in a dusk lit forest surrounded by trees.
+The only direction is deeper into the forest.\n""")
+
+# deep forest area (level 2) setup
+gate1 = Gate("south", start_area)
+deep_forest_area.setup("deep forest", [gate1], "You are in a seemingly endless tunnel of dark oak trees.\n")
 
 
-#defines gates and levels
-gate1 = gate("north", deep_forest_area)
-start_area.setup("forest", [gate1], "You are in a dusk lit forest surrounded by trees. The only direction is deeper into the forest")
+# defines function to loop player for incorrect answers
+def incorrect_answer_loop(answers):
 
-gate1 = gate("south", start_area)
-deep_forest_area.setup("deep forest", [gate1], "You are in a seemingly endless tunnel of dark oak trees")
-
-#defines function to loop player for incorrect answers
-def incorrectAnswerLoop(answers):
     while True:
-        user_input=input()
+        user_input_for_loop = input()
 
-        if user_input in answers:
-            return user_input
-            break
+        if user_input_for_loop in answers:
+            return user_input_for_loop
 
         else:
             print("incorrect input")
             continue
 
-#defines sequence for players sword creation
+
+# defines sequence for players Sword creation
 def sword_sequence():
 
-    #asks user for input/type of sword. Uses while loop to stop incorrect input breaking program
-    print("Type of sword \n katana, claymore, dagger")
+    # defines players sword, adds default values to catch errors
+    player_sword = Sword("claymore", "blood", "red")
 
-    #puts player in loop to collect sword type
-    sword.type = incorrectAnswerLoop({"katana", "claymore", "dagger"})
+    # asks user for input/type of Sword. Uses while loop to stop incorrect input breaking program
+    print("Type of Sword \n katana, claymore, dagger")
 
-    print("\n Cool your sword type is:", sword.type, "\n")
+    # puts player in loop to collect Sword type
+    player_sword.sword_type = incorrect_answer_loop({"katana", "claymore", "dagger"})
 
-    #asks user input and stores as variable, lets player use whatever colour.
-    print("what colour sword?\n")
-    sword.colour=input()
+    print("\n Cool your Sword type is:", player_sword.sword_type, "\n")
+
+    # asks user input and stores as variable, lets player use whatever colour.
+    print("what colour Sword?\n")
+    player_sword.colour = input()
     
-    print("\nCool, your sword colour is:", sword.colour, "\n")
+    print("\nCool, your Sword colour is:", player_sword.colour, "\n")
 
-    #asks user what damage type they want, using a while loop for accepted input
+    # asks user what damage type they want, using a while loop for accepted input
     print("What damage type?\n holy, blood, fire\n")
     
-    #puts player in loop to collect damage type
-    sword.damagetype = incorrectAnswerLoop({"holy", "blood", "fire"})
+    # puts player in loop to collect damage type
+    player_sword.damage_type = incorrect_answer_loop({"holy", "blood", "fire"})
 
-    #prints sword stats to player
-    print("\nCool, your  sword type is", sword.type, ", your sword colour is", sword.colour, ", and your swords damage type is", sword.damagetype)
+    # prints Sword stats to player
+    print("\nCool, your  Sword type is", player_sword.sword_type, ", your Sword colour is",
+          player_sword.colour, ", and your Swords damage type is", player_sword.damage_type)
 
-#defines level (unfinished btw)
-def forest_sequence():
-    #defines commands inorder to print to player
-    commands = ["forward", "left", "right",\
-        "sword stats", "location", "help", "pick up", "attack"]
 
-    #asks players name to use in later dialogue (which is not yet implemented)
-    print("what is your name?")
-    player.name = input()
+# defines commands in list
+commands = ["go (direction)", "look", "name"]
 
-    #outlines area around player and gives help command
-    print("You are in a forest and your sword is next to you.\n (type help for list of commands)")
-
-    #starts while loop inorder to "kill" player when health reaches zero (however combat system not yet implemented)
-    while player.health > 0:
-        #asks for user input
-        user_input = input().lower()
-        
-        #big array of user input possibilities which most are fairly simple
-        if user_input == "help":
-            print("your commands are:", commands)
-
-        elif user_input == "sword stats":
-            print("\nCool, your  sword type is", sword.type, ", your sword colour is", sword.colour, ", and your swords damage type is", sword.damagetype)
-
-        elif user_input == "location":
-            print(player.location)
-
-        #prints string 1 if sword is picked up. String 2 if not
-        elif user_input == "attack":
-            if sword.pickedup == True:
-                print("you swing your sword and hack off a branch")
-            else:
-                print("you dont have your sword")
-
-        #assigns True boolean to sword class if pick up is entered. if already picked up it says you already have your sword
-        elif user_input== "pick up":
-            if sword.pickedup == False and player.location == "start point":
-                print("you pick up your sword")
-                sword.pickedup = True
-            else:
-                print("you already have your sword")
-
-        #inputs other than defined continues loop
-        else:
-            print("incorrect input")
-            continue
-    
-    #prints string if player is dead
-    if player.health > 0:
-        print("You Died")
-    
-    #pass statement to prevent possible errors
-    else:
-        pass
-
-#defines player instance
-the_player = player
-
-#asks name and stores in player instance
+# asks name and stores in player instance
 print("what is your name?")
 user_input = input(">")
-the_player.name = user_input
+the_player = Player(user_input, start_area)
 
-the_player.location = start_area
+# begins sword sequence
+sword_sequence()
+
+# gives player information and area setting
 print("(type help for commands)")
+print(start_area.description)
 
+# starts game loop
 while user_input != "exit":
-    #sword_sequence()
-    print(start_area.description)
-    user_input = input(">")
 
-    for d in the_player.location.directions:
+    user_input = str.lower(input(">"))
+
+    # looks for direction in input and if the direction is valid, moves player in that direction
+    for d in the_player.location.gates:
         if d.is_gate(user_input):
             the_player.move(d.gate_to)
 
+    # tells player the commands when asked
+    if user_input == "help":
+        print("the commands are:", commands)
 
+    # gives player location information
+    elif user_input == "look":
+        print(the_player.location.enter())
 
+    # catches errors, misspells, etc.
     else:
         print("incorrect input")
         continue
