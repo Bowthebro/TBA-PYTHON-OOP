5a4f2f7d9232bc888471e616f5ddae1cb526e9da
Bowthebro <85522775+Bowthebro@users.noreply.github.com>
2022-12-13 16:08:10 +1300

    update for tba.py
    style and logistic changes, sort of works now. However a few bugs need to go before I can start to add items, puzzles etc.

--- Tba.py
+++ Tba.py
@@ -1,11 +1,9 @@
-# TEXT BASED ADVENTURE OOP PYTHON PROGRAM
+# TEXT BASED ADVENTURE. OOP. PYTHON PROGRAM #
 
 # defines Level class
 class Level:
     def __init__(self):
         self.name = ""
-        self.description = None
-        self.gates = None
 
     #  defines level setup function, defines instances of level class
     def setup(self, name, gates, text):
@@ -17,16 +15,23 @@ class Level:
     def enter(self):
         print(self.description)
 
-        if len(self.gates) >= 1:
+        if len(self.gates) == 1:
+            print("there is a gate to ", end=' ')
+
+            gate_str = " "
+            gate_str = gate_str.join(self.gates)
+
+            print(gate_str)
+
+        elif len(self.gates) > 1:
             print("there is a gate to ", end=' ')
             for g in self.gates:
                 g.print_gate()
-        print("\n", end='')
 
 
 # defines gate class
 class Gate:
-    
+
     def __init__(self, direction, gate_to):
         self.gate_to = gate_to
         self.direction = direction
@@ -37,7 +42,7 @@ class Gate:
 
     # defines print gate function, prints direction of gate
     def print_gate(self):
-        print("the %s," % self.direction, end=' ')
+        print("the %s," % self.direction)
 
 
 # defines player class
@@ -69,17 +74,16 @@ deep_forest_area = Level()
 
 # start area setup
 gate1 = Gate("north", deep_forest_area)
-start_area.setup("forest", [gate1], """You are in a dusk lit forest surrounded by trees.
-The only direction is deeper into the forest.\n""")
+start_area.setup("forest", [gate1], "You are in a dusk lit forest surrounded by trees. "
+                                    "The only direction is deeper into the forest.")
 
 # deep forest area (level 2) setup
 gate1 = Gate("south", start_area)
-deep_forest_area.setup("deep forest", [gate1], "You are in a seemingly endless tunnel of dark oak trees.\n")
+deep_forest_area.setup("deep forest", [gate1], "You are in a seemingly endless tunnel of dark oak trees.")
 
 
 # defines function to loop player for incorrect answers
 def incorrect_answer_loop(answers):
-
     while True:
         user_input_for_loop = input()
 
@@ -93,7 +97,6 @@ def incorrect_answer_loop(answers):
 
 # defines sequence for players Sword creation
 def sword_sequence():
-
     # defines players sword, adds default values to catch errors
     player_sword = Sword("claymore", "blood", "red")
 
@@ -108,12 +111,12 @@ def sword_sequence():
     # asks user input and stores as variable, lets player use whatever colour.
     print("what colour Sword?\n")
     player_sword.colour = input()
-    
+
     print("\nCool, your Sword colour is:", player_sword.colour, "\n")
 
     # asks user what damage type they want, using a while loop for accepted input
     print("What damage type?\n holy, blood, fire\n")
-    
+
     # puts player in loop to collect damage type
     player_sword.damage_type = incorrect_answer_loop({"holy", "blood", "fire"})
 
@@ -123,18 +126,18 @@ def sword_sequence():
 
 
 # defines commands in list
-commands = ["go (direction)", "look", "name"]
+commands = ["go (direction)", "look"]
 
 # asks name and stores in player instance
 print("what is your name?")
 user_input = input(">")
 the_player = Player(user_input, start_area)
 
-# begins sword sequence
-sword_sequence()
+# begins sword sequence (disabled for bug testing)
+# sword_sequence()
 
 # gives player information and area setting
-print("(type help for commands)")
+print("(Cry help for commands)\n")
 print(start_area.description)
 
 # starts game loop
@@ -146,6 +149,7 @@ while user_input != "exit":
     for d in the_player.location.gates:
         if d.is_gate(user_input):
             the_player.move(d.gate_to)
+            print(the_player.location.description)
 
     # tells player the commands when asked
     if user_input == "help":
@@ -154,8 +158,3 @@ while user_input != "exit":
     # gives player location information
     elif user_input == "look":
         print(the_player.location.enter())
-
-    # catches errors, misspells, etc.
-    else:
-        print("incorrect input")
-        continue
