74b0457e66393f8497504bbad9142840de3e27f9
Bowthebro <85522775+Bowthebro@users.noreply.github.com>
2023-01-13 17:40:20 +1300

    changed grammar options in gate and item printing. added like 5 levels. started work on lock function for gate. need to work on terminal cleaness/format.

--- Tba.py
+++ Tba.py
@@ -1,4 +1,15 @@
 # TEXT BASED ADVENTURE. OOP. PYTHON PROGRAM #
+#
+# MAP OF GAME
+#                #   #   #  #   #    #  #
+#                #   moldy skeleton     #
+#   #   #   #   #   #   #   ~   #   #   #    #  #
+#   dense shrubs  /  cross roads  /   old tree  #
+#   #   #   #   /   #   #   #   #   #   #   #   #
+#    deep forest    #
+#   #   /   #   #   #
+#     start     #
+#   #   #   #   #
 
 # START OF CLASSES AND INITIALISING
 
@@ -19,31 +30,22 @@ class Level:
 
     # defines enter function, gives player info of area
     def enter(self):
-        print("\n", self.description)
-
-        # for loop to print gates in player location
-        if len(self.gates) > 1:
-            print("There is a gate to", end=' ')
-            for g in self.gates:
-                g.print_gates()
+        print(self.description)
 
         # for loop to print gate in player location
-        elif len(self.gates) == 1:
-            print("There is a gate to", end=' ')
+        if len(self.gates) >= 1:
+            print("There are gates to:")
             for g in self.gates:
                 g.print_gate()
 
         # for loop to print items for use in a for loop
-        if len(self.items) > 1:
-            print("There is", end=' ')
-            for i in self.items:
-                i.print_items()
-
-        # for loop to print item for use in a for loop
-        elif len(self.items) == 1:
-            print("There is", end=' ')
+        if len(self.items) >= 1:
+            print("There is", end=" ")
             for i in self.items:
                 i.print_item()
+
+        # newline so terminal looks better
+        print("\n")
 
     # removes item from level for use in take function
     def remove_item(self, item):
@@ -57,19 +59,16 @@ class Level:
 # defines gate class
 class Gate:
 
-    def __init__(self, direction, gate_to):
+    def __init__(self, direction, gate_to, locked):
         self.gate_to = gate_to
         self.direction = direction
+        self.locked = locked
 
     # returns direction of gate
     def is_gate(self, text):
         return self.direction in text
 
     # prints direction of gate
-    def print_gates(self):
-        print("the %s," % self.direction)
-
-    # second print gate function for grammar reasons
     def print_gate(self):
         print("the %s." % self.direction)
 
@@ -126,10 +125,6 @@ class Item:
     def print_item(self):
         print("a %s." % self.name)
 
-    # function called for use in a for loop to print list of items
-    def print_items(self):
-        print("a %s," % self.name)
-
 
 # defines commands in list
 commands = ["go (direction)", "look", "take (item)", "inventory", "drop (item)"]
@@ -137,23 +132,51 @@ commands = ["go (direction)", "look", "take (item)", "inventory", "drop (item)"]
 # initialises levels
 start_area = Level()
 deep_forest_area = Level()
+cross_road_area = Level()
+moldy_skeleton_area = Level()
+dense_shrubs_area = Level()
+old_tree_area = Level()
 
-# initialises sword instance
+# initialises items
 player_sword = Item("sword")
+rusted_key = Item("rusted key")
 
 # start area setup
-gate1 = Gate("north", deep_forest_area)
+gate1 = Gate("north", deep_forest_area, False)
 start_area.setup("forest", [gate1], "You are in a dusk lit forest surrounded by trees. "
                                     "The only direction is deeper into the forest.", [player_sword])
 
 # deep forest area setup
-gate1 = Gate("south", start_area)
-deep_forest_area.setup("deep forest", [gate1], "You are in a seemingly endless tunnel of dark oak trees.", [])
+gate1 = Gate("south", start_area, False)
+gate2 = Gate("north", cross_road_area, False)
+deep_forest_area.setup("deep forest", [gate1, gate2], "You are in a seemingly endless tunnel of dark oak trees.", [])
+
+# cross road area setup
+gate1 = Gate("south", deep_forest_area, False)
+gate2 = Gate("north", moldy_skeleton_area, True)
+gate3 = Gate("west", dense_shrubs_area, False)
+gate4 = Gate("east", old_tree_area, False)
+cross_road_area.setup("cross road area", [gate2, gate4, gate1, gate3], "You are at a crossroads. "
+                                                                       "The path spirals into three directions."
+                                                                       " It is suddenly dark. ", [])
+
+gate1 = Gate("east", cross_road_area, False)
+dense_shrubs_area.setup("dense shrubs area", [gate1], "You are in an area with dense shrubbery."
+                                                      "The only direction is backwards", [])
+
+gate1 = Gate("west", cross_road_area, False)
+old_tree_area.setup("old tree area", [gate1],
+                    "You see a large old looking tree. Something is hanging off a branch", [rusted_key])
+
+gate1 = Gate("south", cross_road_area, False)
+moldy_skeleton_area.setup("moldy skeleton area", [gate1], "There is a skeleton covered in mold, the path is too"
+                                                          "tight to go around it", [])
 
 
 # END OF CLASSES AND INITIALISING
 
 # START OF FUNCTIONS AND MAIN GAME LOOPS
+
 
 # defines function to loop player for incorrect answers
 def incorrect_answer_loop(answers):
@@ -199,7 +222,7 @@ def sword_sequence():
 
 
 # begins sword sequence
-sword_sequence()
+# sword_sequence()
 
 # asks name and stores in player instance
 print("What is your name.")
@@ -219,12 +242,16 @@ while user_input != "exit":
     # looks for direction in input and if the direction is valid, moves player in that direction
     for d in the_player.location.gates:
         if d.is_gate(user_input):
-            the_player.move(d.gate_to)
-            print("\n", the_player.location.description)
+            if d.locked:
+                print("The gate rattles and doesnt budge.")
+
+            else:
+                the_player.move(d.gate_to)
+                print(the_player.location.description)
 
     # tells player the commands when asked
     if "help" in user_input:
-        print("\nthe commands are:", commands)
+        print("the commands are:", commands)
 
     # gives player location information
     elif "look" in user_input:
